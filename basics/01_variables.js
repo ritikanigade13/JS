ccfceb4c71e02e2daf091c2a4c6ca32fbe4d57c4
ritikanigade13 <89742522+ritikanigade13@users.noreply.github.com>
2024-04-05 19:59:04 +0530

    variables

---
+++ basics/01_variables.js
@@ -0,0 +1,28 @@
+const accountId = 12279
+let accountEmail = "ritika@ymail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+
+// accountId=99373 not allowed
+accountEmail = "nigade@ymail.com"
+accountPassword= "89273"
+accountCity="Indore"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState]);
+/*
+prefer not to use var because of block and functional scope
+outpt:
+┌─────────┬────────────────────┐
+│ (index) │       Values       │
+├─────────┼────────────────────┤
+│    0    │       12279        │
+│    1    │ 'nigade@ymail.com' │
+│    2    │      '89273'       │
+│    3    │      'Indore'      │
+│    4    │     undefined      │
+└─────────┴────────────────────┘
+*/
