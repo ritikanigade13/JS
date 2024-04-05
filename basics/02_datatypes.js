8a855a1a44fbe3e4102498a45db981388f9c5f66
ritikanigade13 <89742522+ritikanigade13@users.noreply.github.com>
2024-04-05 20:25:51 +0530

    datatypes

---
+++ basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) we are using nodejs not browser ie it is not work
+// code readability should be high
+
+console.log(3 + 3);
+console.log("yayyy");
+
+let name = "ritika";
+let age = 22;
+let isLoggedIn = false;
+
+/*types of datatype:
+number = > 2 to power of 53
+bigint
+string --> '' ""
+boolean --> true or false
+null --> standalone value, you can assign it. ie it hase empty value.
+undefined --> when you did not assign value but declared a variable
+symbol --> to find uniqueness it is used
+
+object
+*/
+
+console.log(typeof null);//--> null provides object
